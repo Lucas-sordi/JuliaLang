4e245a91a59954b454565044567a4e3692f1f61c
Lucas Sordi <186245@upf.br>
2021-04-07 09:18:51 -0300

    add other examples

--- Iterações.jl
+++ Iterações.jl
@@ -7,17 +7,8 @@ i = 0
     end
 
 
-#loop for - printar os números impares de 10 a 20 + uso do escape "continue"
-println("--- LOOP FOR - IMPARES DE 10 A 20 USANDO CONTINUE ---")
-for j = 10:20
-    if j % 2 == 0
-        continue
-    end
-     println(j)
-end
-
 #loop while - printar os números de 0 a 20 + uso do escape "break"
-println("--- LOOP WHILE - 0 A 20 USANDO BREAK ---")
+println("\n --- LOOP WHILE - 0 A 20 USANDO BREAK ---")
 k = 0
     while true
         println(k)
@@ -28,10 +19,39 @@ k = 0
      end
 
 
+#loop range for - printar os números impares de 10 a 20 + uso do escape "continue"
+println("\n --- LOOP RANGE FOR - IMPARES DE 10 A 20 USANDO CONTINUE ---")
+    for j = 10:20
+        if j % 2 == 0
+            continue
+        end
+        println(j)
+    end
+
+
+#loop each for - printar todos os elementos pares do array
+    println("\n --- LOOP EACH FOR - ELEMENTOS PARES DO ARRAY ---")
+    for l in [5, 10, 15, 20, 25, 30, 35]
+        if l % 2 == 0
+        println(l)
+        end
+    end
+
+
+
 #escape return usado dentro de uma função
-println("--- FUNÇÃO USANDO O RETURN ---")
-function multiplicacao(x,y)
-    return x * y
-end
+    println("\n --- FUNÇÃO USANDO O RETURN ---")
+    function multiplicacao(x,y)
+        return x * y
+    end
 
 println(multiplicacao(10,20))
+
+
+#loop range for - uma matriz pode ser feita de maneira simples com o range for"
+println("\n --- LOOP FOR - MOSTRAR TODOS OS ELEMENTOS DA MATRIZ 5X5 EM QUE X > Y ---")
+    for m = 1:5, n = 1:5
+        if m > n
+            println((m, n))
+        end
+    end
