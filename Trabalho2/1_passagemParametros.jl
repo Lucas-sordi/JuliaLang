594c5a1d22bde083fae6d0554ca78c0fb8dfa5b6
Leonardo Crestani <leonardo.crestani@hotmail.com>
2021-04-28 14:27:45 -0300

    Adicinado palavras chave

--- Trabalho2/1_passagemParametros.jl
+++ Trabalho2/1_passagemParametros.jl
@@ -6,7 +6,6 @@
 function somatorioNumeros(i, f)
     soma = 0
     for j = i+1:f-1
-        println(j)
         soma = soma + j
     end
     return soma
@@ -15,3 +14,13 @@ end
 # Passamos 1 e 20 como parametros para i e f, respectivamente
 total = somatorioNumeros(1, 20)
 println("Somatorio entre o intervalo dos valores passados para a funcao, igual a  ", + total)
+
+# Passagem por palavra chave
+# Keyword recebe um valor passado pelo parametro keyword
+function f(x; keyword)
+    return x+keyword
+end
+
+# Obrigatorio passar um parametro igual a palavra chave
+total = f(2; keyword=4)
+println("Soma total dos valores ", + total)
